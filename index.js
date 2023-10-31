78d267cd96480a5b361e2382a5be361f72d9ebe2
dbox <danielbox@gmail.com>
2023-10-31 10:12:49 -0400

    add js

--- index.js
+++ index.js
@@ -6,27 +6,65 @@ module.exports = (opts = {}) => {
     postcssPlugin: 'postcss-pxv',
     Once(root, { result }) {
       root.walkDecls((decl) => {
-        const parsedValue = valueParser(decl.value);
+        const propertyName = decl.prop;
 
-        parsedValue.walk((node) => {
-          if (node.type === 'word' && /^[0-9.-]+pxv$/i.test(node.value)) {
-            const pxvValue = parseFloat(node.value.replace('pxv', ''));
+        // Check if the property is a shorthand property like margin
+        const isShorthand = /^(margin|padding|border|border-(top|right|bottom|left))$/.test(propertyName);
 
+        if (isShorthand) {
+          // Split the shorthand value into individual components
+          const components = decl.value.split(' ');
+
+          // Process each component
+          components.forEach((component, index) => {
+            const parsedValue = valueParser(component);
             const basis = 'var(--siteBasis)';
             const max = 'var(--siteMax)';
             const min = '1px';
 
-            if (pxvValue >= 0) {
-              node.value = `clamp(${min}, calc(${pxvValue}vw * (100 / ${basis})), calc(${pxvValue}px * ${max} / ${basis}))`;
-            } else {
-              const absPxvValue = Math.abs(pxvValue);
-              node.value = `clamp(calc(${absPxvValue} * (100 / ${basis}) * -1vw), calc(${absPxvValue} * (100 / ${basis}) * -1vw), -1px)`;
-              // still needs "large" value tweak
-            }
-          }
-        });
+            parsedValue.walk((node) => {
+              if (node.type === 'word' && /^[0-9.-]+pxv$/i.test(node.value)) {
+                const pxvValue = parseFloat(node.value.replace('pxv', ''));
 
-        decl.value = parsedValue.toString();
+                if (pxvValue >= 0) {
+                  node.value = `clamp(${min}, calc(${pxvValue}vw * (100 / ${basis})), calc(${pxvValue}px * ${max} / ${basis}))`;
+                } else {
+                  const absPxvValue = Math.abs(pxvValue);
+                  node.value = `clamp(calc(${absPxvValue} * (100 / ${basis}) * -1vw), calc(${absPxvValue} * (100 / ${basis}) * -1vw), -1px)`;
+                }
+              }
+            });
+
+            // Replace the component in the shorthand value
+            components[index] = parsedValue.toString();
+          });
+
+          // Rejoin the processed components back into the shorthand value
+          decl.value = components.join(' ');
+        } else {
+          // For non-shorthand properties, process the value as before
+          const parsedValue = valueParser(decl.value);
+
+          parsedValue.walk((node) => {
+            if (node.type === 'word' && /^[0-9.-]+pxv$/i.test(node.value)) {
+              const pxvValue = parseFloat(node.value.replace('pxv', ''));
+
+              const basis = 'var(--siteBasis)';
+              const max = 'var(--siteMax)';
+              const min = '1px';
+
+              if (pxvValue >= 0) {
+                node.value = `clamp(${min}, calc(${pxvValue}vw * (100 / ${basis})), calc(${pxvValue}px * ${max} / ${basis}))`;
+              } else {
+                const absPxvValue = Math.abs(pxvValue);
+                node.value = `clamp(calc(${absPxvValue} * (100 / ${basis}) * -1vw), calc(${absPxvValue} * (100 / ${basis}) * -1vw), -1px)`;
+                // still needs "large" value tweak
+              }
+            }
+          });
+
+          decl.value = parsedValue.toString();
+        }
       });
     },
   };
