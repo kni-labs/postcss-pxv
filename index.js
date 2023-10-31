f87185f3daeb493ffcc68941ed9974d85fa345e3
Cooper Floyd <cooperwfloyd@gmail.com>
2023-10-31 09:56:17 -0500

    js cleanup

--- index.js
+++ index.js
@@ -1,58 +1,22 @@
-const postcss = require('postcss');
 const valueParser = require('postcss-value-parser');
 
-module.exports = (opts = {}) => {
+module.exports = () => {
   return {
     postcssPlugin: 'postcss-pxv',
-    Once(root, { result }) {
+    Once(root) {
       root.walkDecls((decl) => {
-        const propertyName = decl.prop;
-
-        // Check if the property is a shorthand property like margin
-        const isShorthand = /^(margin|padding|border|border-(top|right|bottom|left))$/.test(propertyName);
-
-        if (isShorthand) {
-          // Split the shorthand value into individual components
-          const components = decl.value.split(' ');
-
-          // Process each component
-          components.forEach((component, index) => {
-            const parsedValue = valueParser(component);
-            const basis = 'var(--siteBasis)';
-            const max = 'var(--siteMax)';
-            const min = '1px';
-
-            parsedValue.walk((node) => {
-              if (node.type === 'word' && /^[0-9.-]+pxv$/i.test(node.value)) {
-                const pxvValue = parseFloat(node.value.replace('pxv', ''));
-
-                if (pxvValue >= 0) {
-                  node.value = `clamp(${min}, calc(${pxvValue}vw * (100 / ${basis})), calc(${pxvValue}px * ${max} / ${basis}))`;
-                } else {
-                  const absPxvValue = Math.abs(pxvValue);
-                  node.value = `clamp(calc(${absPxvValue} * (100 / ${basis}) * -1vw), calc(${absPxvValue} * (100 / ${basis}) * -1vw), -1px)`;
-                }
-              }
-            });
-
-            // Replace the component in the shorthand value
-            components[index] = parsedValue.toString();
-          });
-
-          // Rejoin the processed components back into the shorthand value
-          decl.value = components.join(' ');
-        } else {
-          // For non-shorthand properties, process the value as before
-          const parsedValue = valueParser(decl.value);
+        const isShorthand = /^(margin|padding)$/.test(decl.prop); // Check if the property is a shorthand property like margin
+        const basis = 'var(--siteBasis)';
+        const max = 'var(--siteMax)';
+        const min = '1px';
+        
+        const convertValue = (value) => {
+          const parsedValue = valueParser(value);
 
           parsedValue.walk((node) => {
             if (node.type === 'word' && /^[0-9.-]+pxv$/i.test(node.value)) {
               const pxvValue = parseFloat(node.value.replace('pxv', ''));
-
-              const basis = 'var(--siteBasis)';
-              const max = 'var(--siteMax)';
-              const min = '1px';
-
+  
               if (pxvValue >= 0) {
                 node.value = `clamp(${min}, calc(${pxvValue}vw * (100 / ${basis})), calc(${pxvValue}px * ${max} / ${basis}))`;
               } else {
@@ -63,7 +27,17 @@ module.exports = (opts = {}) => {
             }
           });
 
-          decl.value = parsedValue.toString();
+          return parsedValue.toString();
+        };
+
+        if (isShorthand) {
+          // Split the shorthand value into individual components. Could potentially be problematic if there are any shorthand values that mix pxv and other values that have spaces like calc, but we'll cross that bridge when we come to it.
+          const components = decl.value.split(' ');
+          // Process each component and rejoin the processed components back into the shorthand value
+          decl.value = components.map((component) => convertValue(component)).join(' ');
+        } else {
+          // For non-shorthand properties, process the value as before
+          decl.value = convertValue(decl.value);
         }
       });
     },
