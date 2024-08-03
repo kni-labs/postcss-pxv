888df3959c4c2851223641f35d2c2c07a6e6b0f0
dbox <danielbox@gmail.com>
2024-08-02 22:27:43 -0500

    i believe this fixes trevor's negative margin edge case

--- index.js
+++ index.js
@@ -8,22 +8,21 @@ module.exports = () => {
         const basis = 'var(--siteBasis)';
         const max = 'var(--siteMax)';
         const min = '1px';
-        
+
         const convertValue = (value) => {
           const parsedValue = valueParser(value);
 
           parsedValue.walk((node) => {
             if (node.type === 'word' && /^[0-9.-]+pxv$/i.test(node.value)) {
               const pxvValue = parseFloat(node.value.replace('pxv', ''));
-  
+
               if (pxvValue === 0) {
-                node.value = 0;
+                node.value = '0';
               } else if (pxvValue > 0) {
                 node.value = `clamp(${min}, calc(${pxvValue}vw * (100 / ${basis})), calc(${pxvValue}px * ${max} / ${basis}))`;
               } else {
                 const absPxvValue = Math.abs(pxvValue);
-                node.value = `clamp(calc(${absPxvValue} * (100 / ${basis}) * -1vw), calc(${absPxvValue} * (100 / ${basis}) * -1vw), -1px)`;
-                // still needs "large" value tweak
+                node.value = `clamp(calc(-${absPxvValue} * (100 / ${basis}) * 1vw), calc(-${absPxvValue}px * ${max} / ${basis}), -${min})`;
               }
             }
           });
@@ -31,7 +30,7 @@ module.exports = () => {
           return parsedValue.toString();
         };
 
-        decl.value = decl.value.split(' ').map((component) => convertValue(component)).join(' ');
+        decl.value = convertValue(decl.value);
       });
     },
   };
