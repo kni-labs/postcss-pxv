e41e2fbef81abb878baa4251c46673e53ae28aa5
Daniel Box <danielbox@gmail.com>
2025-09-26 14:23:04 -0400

    Update index.js
    trying to fix 1px thing

--- index.js
+++ index.js
@@ -27,31 +27,32 @@ module.exports = () => {
       ensureVar('--siteBasis', '375');
       ensureVar('--siteMax', '600');
       ensureVar(
-        '--pxvUnit',
-        'clamp(0px, calc((100 / var(--siteBasis)) * 1vw), calc(1px * var(--siteMax) / var(--siteBasis)))'
+        '--pxvUnitPos',
+        'clamp(1px, calc((100 / var(--siteBasis)) * 1vw), calc(1px * var(--siteMax) / var(--siteBasis)))'
       );
 
       // Walk declarations for pxv replacement
       root.walkDecls((decl) => {
-        const convertValue = (value) => {
-          const parsedValue = valueParser(value);
+        const parsedValue = valueParser(decl.value);
 
-          parsedValue.walk((node) => {
-            if (node.type === 'word' && /^[0-9.-]+pxv$/i.test(node.value)) {
-              const pxvValue = parseFloat(node.value.replace('pxv', ''));
+        parsedValue.walk((node) => {
+          if (node.type === 'word' && /^[0-9.-]+pxv$/i.test(node.value)) {
+            const pxvValue = parseFloat(node.value.replace('pxv', ''));
 
-              if (pxvValue === 0) {
-                node.value = '0';
-              } else {
-                node.value = `calc(${pxvValue} * var(--pxvUnit))`;
-              }
+            if (pxvValue === 0) {
+              node.value = '0';
+            } else if (pxvValue > 0) {
+              // Positive: use clamped min of 1px
+              node.value = `calc(${pxvValue} * var(--pxvUnitPos))`;
+            } else {
+              // Negative: still scale, but keep sign
+              const absVal = Math.abs(pxvValue);
+              node.value = `calc(-${absVal} * var(--pxvUnitPos))`;
             }
-          });
+          }
+        });
 
-          return parsedValue.toString();
-        };
-
-        decl.value = convertValue(decl.value);
+        decl.value = parsedValue.toString();
       });
     },
   };
